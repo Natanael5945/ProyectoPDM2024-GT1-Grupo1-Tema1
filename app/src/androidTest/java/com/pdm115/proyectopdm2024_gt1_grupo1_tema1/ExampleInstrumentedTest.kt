effff2c89bd8941bdf8d09cf270162787808c2f1
Antonio Natanael <cs21062@ues.edu.sv>
2024-05-17 11:16:28 -0600

    Primer commit: Carga del proyecto al repositorio de Github

---
+++ app/src/androidTest/java/com/pdm115/proyectopdm2024_gt1_grupo1_tema1/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.pdm115.proyectopdm2024_gt1_grupo1_tema1
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.pdm115.proyectopdm2024_gt1_grupo1_tema1", appContext.packageName)
+    }
+}
---
+++ app/src/test/java/com/pdm115/proyectopdm2024_gt1_grupo1_tema1/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.pdm115.proyectopdm2024_gt1_grupo1_tema1
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
